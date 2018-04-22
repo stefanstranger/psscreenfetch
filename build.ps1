b11911f969c2dee4528e2cafb79460fad49d3a06
Stefan Stranger <stefan@stranger.nl>
2018-04-22 15:41:11 +0200

    Renamed Test ffolder and added repository info to build script

--- build.ps1
+++ build.ps1
@@ -2,69 +2,59 @@
 #requires -runasadministrator
 
 #Check if not in psdrive folder. Can cause issues with Pester Output tests.
-if ($pwd -cmatch '[A-Z]:') 
-{
-  # Successful match
+if ($pwd -cmatch '[A-Z]:') {
+    # Successful match
 }
-else 
-{
-  Write-Error -Message 'Please don''t use PSDrives'
-  exit
-} 
+else {
+    Write-Error -Message 'Please don''t use PSDrives'
+    exit
+}
 
 
 
-function Resolve-Module
-{
-  [Cmdletbinding()]
-  param
-  (
-    [Parameter(Mandatory)]
-    [string[]]$Name
-  )
+function Resolve-Module {
+    [Cmdletbinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [string[]]$Name
+    )
 
-  Process
-  {
-    foreach ($ModuleName in $Name)
-    {
-      $Module = Get-Module -Name $ModuleName -ListAvailable
-      Write-Verbose -Message "Resolving Module $($ModuleName)"
-            
-      if ($Module) 
-      {                
-        $Version = $Module |
-        Measure-Object -Property Version -Maximum |
-        Select-Object -ExpandProperty Maximum
-        $GalleryVersion = Find-Module -Name $ModuleName -Repository PSGallery |
-        Measure-Object -Property Version -Maximum |
-        Select-Object -ExpandProperty Maximum
+    Process {
+        foreach ($ModuleName in $Name) {
+            $Module = Get-Module -Name $ModuleName -ListAvailable
+            Write-Verbose -Message "Resolving Module $($ModuleName)"
 
-        if ($Version -lt $GalleryVersion)
-        {
-          if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') 
-          {
-            Set-PSRepository -Name PSGallery -InstallationPolicy Trusted 
-          }
-                    
-          Write-Verbose -Message "$($ModuleName) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($GalleryVersion.tostring())]"
-                    
-          Install-Module -Name $ModuleName -Force -SkipPublisherCheck #skip
-          Import-Module -Name $ModuleName -Force -RequiredVersion $GalleryVersion
+            if ($Module) {
+                $Version = $Module |
+                    Measure-Object -Property Version -Maximum |
+                    Select-Object -ExpandProperty Maximum
+                $GalleryVersion = Find-Module -Name $ModuleName -Repository PSGallery |
+                    Measure-Object -Property Version -Maximum |
+                    Select-Object -ExpandProperty Maximum
+
+                if ($Version -lt $GalleryVersion) {
+                    if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {
+                        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+                    }
+
+                    Write-Verbose -Message "$($ModuleName) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($GalleryVersion.tostring())]"
+
+                    Install-Module -Name $ModuleName -Force -SkipPublisherCheck -Repository PSGallery #skip
+                    Import-Module -Name $ModuleName -Force -RequiredVersion $GalleryVersion
+                }
+                else {
+                    Write-Verbose -Message "Module Installed, Importing $($ModuleName)"
+                    Import-Module -Name $ModuleName -Force -RequiredVersion $Version
+                }
+            }
+            else {
+                Write-Verbose -Message "$($ModuleName) Missing, installing Module"
+                Install-Module -Name $ModuleName -Force
+                Import-Module -Name $ModuleName -Force -RequiredVersion $Version
+            }
         }
-        else
-        {
-          Write-Verbose -Message "Module Installed, Importing $($ModuleName)"
-          Import-Module -Name $ModuleName -Force -RequiredVersion $Version
-        }
-      }
-      else
-      {
-        Write-Verbose -Message "$($ModuleName) Missing, installing Module"
-        Install-Module -Name $ModuleName -Force
-        Import-Module -Name $ModuleName -Force -RequiredVersion $Version
-      }
     }
-  }
 }
 
 # Grab nuget bits, install modules, set build variables, start build.
